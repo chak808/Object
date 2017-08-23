fa78705a348e097db7b4063f44694423c621a3b2
chak808 <30875250+chak808@users.noreply.github.com>
2017-08-23 09:18:18 -1000

    Create Area.js

---
+++ Area.js
@@ -0,0 +1,8 @@
+function calArea(width, height){
+	var area = width*height;
+  return area;
+ }
+var x = prompt("width?")
+var y = prompt("hight?")
+var wallOne = calArea(x,y);
+window.alert(wallOne);
